906623573d99c70f9fa50593daa633d8e4a73d7f
DianAN174 <75937087+DianAN174@users.noreply.github.com>
2021-06-01 10:12:47 +0700

    Update ApiAuthController.php

--- app/Http/Controllers/Auth/ApiAuthController.php
+++ app/Http/Controllers/Auth/ApiAuthController.php
@@ -10,59 +10,77 @@ use Illuminate\Support\Str;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
+use App\Utils\Response;
+use Mockery\Exception;
+
 class ApiAuthController extends Controller
 {
-    public function register (Request $request) {
-        $validator = Validator::make($request->all(), [
-            'name' => 'required|string|max:255',
-            'email' => 'required|string|email|max:255|unique:users',
-            'password' => 'required|string|min:6',
-
-        ]);
-        if ($validator->fails())
-        {
-            return response(['errors'=>$validator->errors()->all()], 422);
-        }
-        $request['password']=Hash::make($request['password']);
-        $request['remember_token'] = Str::random(10);
-        //$request['type'] = $request['type'] ? $request['type']  : 0;
-        $user = User::create($request->toArray());
-        //dd($user);
-        $token = $user->createToken('Laravel Password Grant Client')->accessToken;
-        $response = ['token' => $token];
-        return response($response, 200);
-    }
-
-    public function login (Request $request) {
-        $validator = Validator::make($request->all(), [
-            'email' => 'required|string|email|max:255',
-            'password' => 'required|string|min:6',
-        ]);
-        if ($validator->fails())
-        {
-            return response(['errors'=>$validator->errors()->all()], 422);
-        }
-        $user = User::where('email', $request->email)->first();
-        if ($user) {
-            if (Hash::check($request->password, $user->password)) {
-                $token = $user->createToken('Laravel Password Grant Client')->accessToken;
-                $response = ['token' => $token];
-                return response($response, 200);
-            } else {
-                $response = ["message" => "Password mismatch"];
-                return response($response, 422);
+    public function register(Request $request)
+    {
+        try {
+            $validator = Validator::make($request->all(), [
+                'name' => 'required|string|max:255',
+                'email' => 'required|string|email|max:255|unique:users',
+                'password' => 'required|string|min:6',
+                
+            ]);
+            if ($validator->fails()) {
+                return response(['errors' => $validator->errors()->all()], 422);
             }
-        } else {
-            $response = ["message" =>'User does not exist'];
-            return response($response, 422);
+            $request['password'] = Hash::make($request['password']);
+            $request['remember_token'] = Str::random(10);
+
+            $user = User::create($request->toArray());
+
+            $token = $user->createToken('Laravel Password Grant Client')->accessToken;
+            $response = ['token' => $token];
+
+            return Response::HttpResponse(200, $response, "Success Login", false);
+
+        } catch (Exception $e) {
+            return Response::HttpResponse(500, ['errors' => $e->getTraceAsString()], "Internal Server Errorr", false);
+        }
+
+    }
+
+    public function login(Request $request)
+    {
+        try {
+            $validator = Validator::make($request->all(), [
+                'email' => 'required|string|email|max:255',
+                'password' => 'required|string|min:6',
+            ]);
+            if ($validator->fails()) {
+                return Response::HttpResponse(422, ['errors' => $validator->errors()->all()], "Failed Login", false);
+            }
+            $user = User::where('email', $request->email)->first();
+            if ($user) {
+                if (Hash::check($request->password, $user->password)) {
+                    $token = $user->createToken('Laravel Password Grant Client')->accessToken;
+                    $response = ['token' => $token];
+                    return Response::HttpResponse(200, $response, "Success Login", false);
+                } else {
+                    $response = null;
+                    return Response::HttpResponse(422, $response, "Password mismatch", false);
+                }
+            } else {
+                $response = null;
+                return Response::HttpResponse(422, $response, "User does not exist", false);
+            }
+        } catch (Exception $e) {
+            return Response::HttpResponse(500, ['errors' => $e->getTraceAsString()], "Internal Server Errorr", false);
         }
     }
 
-    public function logout (Request $request) {
-        $token = $request->user()->token();
-        $token->revoke();
-        
-        $response = ['message' => 'You have been successfully logged out!'];
-        return response($response, 200);
+    public function logout(Request $request)
+    {
+        try {
+            $token = $request->user()->token();
+            $token->revoke();
+
+            return Response::HttpResponse(422, null, "You have been successfully logged out!", false);
+        } catch (Exception $e) {
+            return Response::HttpResponse(500, ['errors' => $e->getTraceAsString()], "Internal Server Errorr", false);
+        }
     }
 }
