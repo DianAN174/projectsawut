9518fd90e83ffd262e575769a272d079642ac1a0
DianAN174 <75937087+DianAN174@users.noreply.github.com>
2021-06-14 17:48:01 +0700

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -21,12 +21,13 @@ Route::group(['middleware' => ['json.response']], function () {
 });
 
 Route::middleware('auth:api')->group(function () {
-    
+
     Route::get('/auth/user', function (Request $request) {
         return $request->user();
     });
     Route::post('/logout', 'App\Http\Controllers\Auth\ApiAuthController@logout')->name('logout.api');
 
+    
     //dashboard
     Route::get('/wakaf/', 'App\Http\Controllers\Wakaf\Dashboard@Dashboard');
 
