57afc7775dd31bbb7a3c21b4d515124d83bcc268
DianAsihN <dian.a.n@mail.ugm.ac.id>
2021-06-17 10:45:40 +0700

    update KasTable di Pengelolaan

--- app/Http/Controllers/Wakaf/PengelolaanWakaf.php
+++ app/Http/Controllers/Wakaf/PengelolaanWakaf.php
@@ -247,27 +247,27 @@ class PengelolaanWakaf
         try
         {
             $sumKasTunai = KasTunai::sum('saldo');
-            $sumKreditTunai = KasTunai::where('type','=','pengeluaran')->sum('saldo');
+            $sumKreditTunai = KasTunai::where('type','pengeluaran')->sum('saldo');
             $saldoTerakhirKasTunai=$sumKasTunai - $sumKreditTunai;
 
             //ktw=kas tabungan wakaf
             $sum_ktw = KasTabWakaf::sum('saldo');
-            $sumKredit_ktw = KasTabWakaf::where('type','=','pengeluaran')->sum('saldo');
+            $sumKredit_ktw = KasTabWakaf::where('type','pengeluaran')->sum('saldo');
             $saldoTerakhir_ktw=$sum_ktw - $sumKredit_ktw;
 
             //ktbh=kas tabungan bagi hasil
             $sum_ktbh = KasTabBagiHasil::sum('saldo');
-            $sumKredit_ktbh = KasTabBagiHasil::where('type','=','pengeluaran')->sum('saldo');
+            $sumKredit_ktbh = KasTabBagiHasil::where('type','pengeluaran')->sum('saldo');
             $saldoTerakhir_ktbh=$sum_ktbh - $sumKredit_ktbh;
 
             //ktnbh=kas tabungan non bagi hasil
             $sum_ktbnh = KasTabNonBagiHasil::sum('saldo');
-            $sumKredit_ktbnh = KasTabNonBagiHasil::where('type','=','pengeluaran')->sum('saldo');
+            $sumKredit_ktbnh = KasTabNonBagiHasil::where('type','pengeluaran')->sum('saldo');
             $saldoTerakhir_ktbnh = $sum_ktbnh - $sumKredit_ktbnh;
 
             //kdw = kas deposito wakaf
             $sum_kdw = KasDepositoWakaf::sum('saldo');
-            $sumKredit_kdw = KasDepositoWakaf::where('type','=','pengeluaran')->sum('saldo');
+            $sumKredit_kdw = KasDepositoWakaf::where('type','pengeluaran')->sum('saldo');
             $saldoTerakhir_kdw = $sum_kdw - $sumKredit_kdw;
 
             $saldo_terakhir_kas = [$saldoTerakhirKasTunai, $saldoTerakhir_ktw, $saldoTerakhir_ktbh, $saldoTerakhir_ktbnh, $saldoTerakhir_kdw];
